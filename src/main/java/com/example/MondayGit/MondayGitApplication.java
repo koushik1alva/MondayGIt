840164dbcde4007e4e8ebafeaeb27878e9acabd8
Koushik <koushik.k@fireflink.com>
2023-04-24 09:56:56 +0530

    src and pom.xml added

---
+++ src/main/java/com/example/MondayGit/MondayGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.MondayGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MondayGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MondayGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MondayGit/MondayGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MondayGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MondayGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
