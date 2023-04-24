197afc7816958a3681460642a551ab7e007a475d
Koushik <koushik.k@fireflink.com>
2023-04-24 10:15:55 +0530

    DTO and DAO are added

---
+++ src/main/java/com/example/MondayGit/DTO/Person.java
@@ -0,0 +1,9 @@
+package com.example.MondayGit.DTO;
+
+public class Person {
+    private int id;
+    private String name;
+    private String place;
+
+}
+
---
+++ src/main/java/com/example/MondayGit/Dao/PersonDao.java
@@ -0,0 +1,4 @@
+package com.example.MondayGit.Dao;
+
+public class PersonDao {
+}
